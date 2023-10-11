978c82a1f014740115e7f4690d578210a8cf6cd7
Firox <86130692+Firoxus@users.noreply.github.com>
2023-10-11 16:54:25 +0200

    Update ElixiumLib.lua

--- ElixiumLib.lua
+++ ElixiumLib.lua
@@ -1,5 +1,32 @@
 -- // CREDITS TO MANA64 FOR THIS UI LIB <3
 -- // UI LIB DISCORD SERVER: https://discord.gg/NZC6Ja4xqG
+
+getgenv().Settings = {
+    Enabled = false,
+    Boxes = false,
+    Color = Color3.new(135, 206, 250),
+    Width = 3000, -- cool feature
+    Filled = false,
+    FillColor = Color3.fromRGB(0, 0, 0),
+    Transparency = 0.2, -- Filled box transparency
+    Healthbar = false, 
+    Tracers = false,
+    Tcolor = Color3.fromRGB(135, 206, 250), -- Trace color
+    Tfrom = 2, -- where tracers locate
+    TeamCheck = false,
+    TeamColor = false,
+    FriendlyColor = Color3.fromRGB(135, 206, 250), -- If teamcolor on set ur friendlies colors
+    Lac = false, -- Link all colors
+    Lc = Color3.fromRGB(135, 206, 250), -- Linked color
+    Names = false, -- Yeah
+    NameC = Color3.fromRGB(135, 206, 250), -- Name color
+    TextSize = 20, -- Yh
+    Offset = 30, -- Bit bugged cos further u are the higher it goes 
+    Rainbow = false -- Rainbow esp coloer (not filled / outlines)
+}
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BlueButOutlined/Esps/main/Library'))()
+
 if game.CoreGui:FindFirstChild("Neverlose1") then
     game.CoreGui.Neverlose1:Destroy()
 end
