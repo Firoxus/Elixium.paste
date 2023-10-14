56fddd1796daaf17590f1c8b0bcda0d63af46617
Firox <86130692+Firoxus@users.noreply.github.com>
2023-10-14 19:32:12 +0200

    Create ishowthemeattomakeyoufeelcomplete.lua

---
+++ ishowthemeattomakeyoufeelcomplete.lua
@@ -0,0 +1,24 @@
+--// Break in
+if game.PlaceId == 4620170611 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/break%20in.lua"))()
+    end
+    if game.PlaceId == 3851622790 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/break%20in.lua"))()
+    end
+    
+    --// Blade Balls
+    if game.PlaceId == 13772394625 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/bladeballs.lua"))()
+        end
+    
+--// Dahood
+if game.PlaceId == 2788229376 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/da%20hood.lua"))()
+    end
+
+    --//  Rushpoint
+    if game.PlaceId == 7113341058 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/rushpoint.lua"))()
+        end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/universal.lua"))()
