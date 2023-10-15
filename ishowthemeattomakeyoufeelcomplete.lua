e312d174a0a85e9959f4b658e63cac4ae1b617c8
Firox <86130692+Firoxus@users.noreply.github.com>
2023-10-15 12:57:03 +0200

    Update ishowthemeattomakeyoufeelcomplete.lua

--- ishowthemeattomakeyoufeelcomplete.lua
+++ ishowthemeattomakeyoufeelcomplete.lua
@@ -1,24 +1,17 @@
 --// Break in
 if game.PlaceId == 4620170611 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/break%20in.lua"))()
-    end
-    if game.PlaceId == 3851622790 then
+elseif game.PlaceId == 3851622790 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/break%20in.lua"))()
-    end
-    
-    --// Blade Balls
-    if game.PlaceId == 13772394625 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/bladeballs.lua"))()
-        end
-    
+--// Blade Balls
+elseif game.PlaceId == 13772394625 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/bladeballs.lua"))()
 --// Dahood
-if game.PlaceId == 2788229376 then
+elseif game.PlaceId == 2788229376 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/da%20hood.lua"))()
-    end
-
-    --//  Rushpoint
-    if game.PlaceId == 7113341058 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/rushpoint.lua"))()
-        end
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/universal.lua"))()
+--// Rushpoint
+elseif game.PlaceId == 7113341058 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/rushpoint.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Firoxus/Elixium.paste/main/Scripts/universal.lua"))()
+end
